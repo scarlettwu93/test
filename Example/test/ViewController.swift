979cc706ba53ccb8e9cbb366d83f0580c5edb9bc
Zhixuan Lai <zhxnlai@gmail.com>
2016-04-28 12:15:23 -0700

    changing label text

--- Example/test/ViewController.swift
+++ Example/test/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
         swipeableView.nextView = {
             let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
             label.center = swipeableView.center
-            label.text = "Test"
+            label.text = "alksdjflksjdfklsdjfj"
             return label;
             
         }
